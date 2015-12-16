ab7dc45cd024e43a1ec26763a8a10835fc457b04
birneysky <birneysky@163.com>
2015-12-16 18:10:14 +0800

    modify

---
+++ Helper/H264Encoder.h
@@ -0,0 +1,19 @@
+//
+//  H264Encoder.h
+//  SampleBufferDisplay
+//
+//  Created by zhangguang on 15/12/16.
+//  Copyright © 2015年 com.v2tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@protocol h264EncoderDelegate <NSObject>
+
+
+@end
+
+@interface H264Encoder : NSObject
+
+@end
---
+++ Helper/H264Encoder.m
@@ -0,0 +1,13 @@
+//
+//  H264Encoder.m
+//  SampleBufferDisplay
+//
+//  Created by zhangguang on 15/12/16.
+//  Copyright © 2015年 com.v2tech. All rights reserved.
+//
+
+#import "H264Encoder.h"
+
+@implementation H264Encoder
+
+@end
